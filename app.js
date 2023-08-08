8817132701805394dbeeb43f3f82c181cc14a71e
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-08-08 17:24:15 +0700

    finish 4.2 - create app module

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+//@ts-ignore
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
