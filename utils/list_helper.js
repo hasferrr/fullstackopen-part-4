c44d5eb756be4005401d81acfa9072e979b48ed0
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-08-08 21:22:00 +0700

    finish 4,5*

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,7 +4,19 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => blogs.reduce((acc, blog) => acc + blog.likes, 0)
 
+const favoriteBlog = (blogs) => {
+  let mostLikes = { likes: -1 }
+  for (let i = 0; i < blogs.length; i++) {
+    const obj = blogs[i]
+    if (obj.likes > mostLikes.likes) {
+      mostLikes = { title: obj.title, author: obj.author, likes: obj.likes }
+    }
+  }
+  return mostLikes.likes === -1 ? {} : mostLikes
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
